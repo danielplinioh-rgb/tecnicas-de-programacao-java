8b9730ce599a77341c63c6a872f9164f6b86b522
Daniel Plinio <danielplinioh@gmail.com>
2026-08-15 01:30:58 +0000

    feat: resolvendo exercício 3 - exibindo ficha do aluno formatada

---
+++ src/lista01/Exercicio03.java
@@ -0,0 +1,13 @@
+package lista01;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        System.out.println("============================");
+        System.out.println("       FICHA DO ALUNO       ");
+        System.out.println("============================");
+        System.out.println("Nome:  Daniel Plinio");
+        System.out.println("Curso: Tecnologia em Desenvolvimento de Software Multiplataforma");
+        System.out.println("Turma: Turma do Pagode - Noturno");
+        System.out.println("============================");
+    }
+}
