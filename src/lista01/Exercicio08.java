b4afed9716f3e1731c9d8b624a070e0e1d9c1eec
Daniel Plinio <danielplinioh@gmail.com>
2026-08-15 02:59:24 +0000

    feat: resolvendo exercício 8 - corrigindo declarações

---
+++ src/lista01/Exercicio08.java
@@ -0,0 +1,21 @@
+package lista01;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        //Declarações devidamente corrigidas
+        int idade = 20;
+        double altura = 1.75;
+        char letra = 'A';
+        boolean matriculado = true;
+
+        //Exibindo os valores corrigidos
+        System.out.println("==============================");
+        System.out.println("     DECLARAÇÕES CORRIGIDAS   ");
+        System.out.println("==============================");
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Letra: " + letra);
+        System.out.println("matriculado: " + matriculado);
+        System.out.println("==============================");
+    }
+}
