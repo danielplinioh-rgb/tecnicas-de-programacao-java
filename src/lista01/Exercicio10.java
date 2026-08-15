4132214924fd91cfd50f8481d2423ffde937c5f8
Daniel Plinio <danielplinioh@gmail.com>
2026-08-15 18:21:14 +0000

    feat: resolvendo o exercicio 10 - calculando média aritmética das notas

---
+++ src/lista01/Exercicio10.java
@@ -0,0 +1,17 @@
+package lista01;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+    //Declarando variáveis para armazenamento das notas
+    double nota1 = 10;
+    double nota2 = 8.6;
+    double nota3 = 7.5;
+
+    //Calculando média aritmética
+    double media = (nota1 + nota2 + nota3) / 3;
+
+    //Exibindo média das notas
+    System.out.printf("A média do aluno é: %.1f%n", media);
+
+    }
+}
