3f14410458dc85a6d9d16afc897337956c43bcd4
Daniel Plinio <danielplinioh@gmail.com>
2026-08-15 02:28:43 +0000

    feat: resolvendo exercício 6 - exibindo frase concatenada

---
+++ src/lista01/Exercicio6.java
@@ -0,0 +1,12 @@
+package lista01;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        //Declaração e inicialização das variáveis
+        String nome = "Marina";
+        int idade = 20;
+
+        //Concatenando valores para exibir frase
+        System.out.println(nome + " possui " + idade  + " anos.");
+    }
+}
