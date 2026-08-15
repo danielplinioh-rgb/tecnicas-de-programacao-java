4c12d57b163019c18d2d5d6f3047c7ec9483514d
Daniel Plinio <danielplinioh@gmail.com>
2026-08-15 01:45:17 +0000

    feat: resolvendo exercício 4 - explicando a diferença entre 'print' e 'println' em Java!

---
+++ src/lista01/Exercicio04.java
@@ -0,0 +1,9 @@
+package lista01;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        System.out.println("Explicando a diferença entre 'print' e 'println' em Java!");
+        System.out.println("Print: Serve para indicar ao algoritmo que exiba a frase na mesma linha, sem pular para a próxima.");
+        System.out.println("Println: Indica ao algoritmo que ele deve exibir a frase e pular para a próxima linha.");
+    }   
+}
