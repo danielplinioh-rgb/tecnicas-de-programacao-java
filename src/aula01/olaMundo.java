7d50e48bd061875bccb8261fdddf7990bc49408f
Daniel Plinio <danielplinioh@gmail.com>
2026-08-15 00:29:03 +0000

    feat: resolvendo o exercício 01

--- src/aula01/olaMundo.java
+++ src/aula01/olaMundo.java
@@ -1,3 +1,4 @@
+package aula01;
 public class olaMundo {
     public static void main(String[] args) {
         System.out.println("Olá, GitHub Codespace! Tá rodando lisinkk");
---
+++ src/lista01/Exercicio01.java
@@ -0,0 +1,8 @@
+package lista01;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        System.out.println("Nome Daniel Plinio");
+        System.out.println("Curso: Tecnologia em Desenvolvimento de Software Multiplataforma");
+    }
+}
