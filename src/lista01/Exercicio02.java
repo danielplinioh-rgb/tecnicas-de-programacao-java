2311ec19a5f063b5acbd0789908e3d404fe8ca03
Daniel Plinio <danielplinioh@gmail.com>
2026-08-15 01:16:04 +0000

    feat: resolvendo o exercício 2 - exibindo 3 mensagens

--- src/lista01/Exercicio02.java
+++ src/lista01/Exercicio02.java
@@ -1,5 +1,9 @@
 package lista01;
 
 public class Exercicio02 {
-    
+    public static void main(String[] args) {
+        System.out.println("Primeira mensagem: Força,");
+        System.out.println("Segunda mensagem: Foco,");
+        System.out.println("Terceira mensagem: Fé. ");
+    }
 }
