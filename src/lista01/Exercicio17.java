5995052fb5898a7b5ffc4cc6911102ff5b687c34
Daniel Plinio <danielplinioh@gmail.com>
2026-08-17 02:53:13 +0000

    feat: resolvendo o exercício 17 -  manipulando strings com toUpperCase e length

--- src/lista01/Exercicio17.java
+++ src/lista01/Exercicio17.java
@@ -4,10 +4,26 @@ package lista01;
 
     public class Exercicio17 {
     
-        public static void main(String[] args) {
-            
-        //Instanciando leitor do teclado
+        public static void main(String[] args) {    
         Scanner scanner = new Scanner(System.in);
-        
-        }
+
+        //Capturando nome e sobrenome
+        System.out.print("Digite seu primeiro nome: ");
+        String nome = scanner.next();
+
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        //Manipulação das Strings
+        String nomeCompleto = nome + " " + sobrenome;
+        String nomeMaiusculo = nomeCompleto.toUpperCase();
+        int quantidadeCaracteres = nomeCompleto.length();
+
+        //Exibição dos dados capturados
+        System.out.printf("Nome completo: %s%n", nomeCompleto);
+        System.out.printf("Nome completo (em maiúsculo): %s%n", nomeMaiusculo);
+        System.out.printf("Quantidade de caractéres (contando com espaço): %d%n", quantidadeCaracteres);                                                       
+
+        scanner.close();
+    }
 }
